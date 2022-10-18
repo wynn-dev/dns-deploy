eec8293c0794a8c738dd60a751e01ea5e11748be
wynn <209275660@qq.com>
2022-10-18 14:07:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ echo -e "nameserver 1.1.1.1" >> /etc/resolv.conf
 mkdir -p ~/docker-compose/pihole
 cd ~/docker-compose/pihole
 
-wget 
+wget https://raw.githubusercontent.com/wynn-dev/dns-deploy/main/docker-compose.yml
 
 docker-compose up -d
